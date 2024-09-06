3ee1932771a502a2e9cd68f8876a047fec69f413
Raphael <115659111+VCliqu3@users.noreply.github.com>
2024-09-06 17:26:36 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Semana3___Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Controller controller = new Controller();
+            controller.Execute();
+        }
+    }
+}
